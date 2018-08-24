9ec85a63117d8b311c65bf2a2a9a9451f8b3bcd4
student <llukechadwick@gmail.com>
2018-08-24 15:23:56 +1200

    new data seed

--- seeds/students.js
+++ seeds/students.js
@@ -2,13 +2,13 @@ exports.seed = function (knex, Promise) {
   return knex('students').del()
     .then(function () {
       return knex('students').insert([
-        {name: '0Ambitious Aardvark', email: 'aardvark@example.org', phone: '1234', instrument_id: 0, bio: 'hi my name is...', location_id: 1, image_url: '1.jpg'},
-        {name: '0Bamboozled Baboon', email: 'baboon@example.org', phone: '1234', instrument_id: 0, bio: 'hi my name is...', location_id: 3, image_url: '1.jpg'},
-        {name: '0Curious Capybara', email: 'capybara@example.org', phone: '1234', instrument_id: 0, bio: 'hi my name is...', location_id: 4, image_url: '1.jpg'},
-        {name: '0Dilapidated Duck', email: 'duck@example.org', phone: '1234', instrument_id: 0, bio: 'hi my name is...', location_id: 2, image_url: '1.jpg'},
-        {name: '0Exuberant Elephant', email: 'elephant@example.org', phone: '1234', instrument_id: 0, bio: 'hi my name is...', location_id: 1, image_url: '1.jpg'},
-        {name: '0Fascinated Flying Fox', email: 'flying.fox@example.org', phone: '1234', instrument_id: 0, bio: 'hi my name is...', location_id: 3, image_url: '1.jpg'},
-        {name: '0Generous Gila Monster', email: 'gila.monster@example.org', phone: '1234', instrument_id: 0, bio: 'hi my name is...', location_id: 2, image_url: '1.jpg'},
+        {name: 'Sandy Beaches', email: 'sandy@example.org', phone: '021894981', instrument_id: 1, bio: 'hi my name is Sandy, I like to go to the beach', location_id: 1, image_url: '1.jpg'},
+        {name: 'Json Statham', email: 'json@example.org', phone: '022198189', instrument_id: 2, bio: 'hi my name is Json', location_id: 3, image_url: '1.jpg'},
+        {name: 'Sylvester Schwarzenegger', email: 'sylvester@example.org', phone: '021289498', instrument_id: 1, bio: 'hi my name is Sylvester', location_id: 4, image_url: '1.jpg'},
+        {name: 'Bob Smith', email: 'bobs@example.org', phone: '045929', instrument_id: 3, bio: 'hi my name is Bob', location_id: 2, image_url: '1.jpg'},
+        {name: 'Bob Dylan', email: 'bobd@example.org', phone: '04981894189', instrument_id: 1, bio: 'hi my name is Bob', location_id: 1, image_url: '1.jpg'},
+        {name: 'Eddie Vedder', email: 'flylikeavedder@example.org', phone: '0428945', instrument_id: 3, bio: 'hi my name is Eddie', location_id: 3, image_url: '1.jpg'},
+        {name: 'Madonna', email: 'madonna@example.org', phone: '015198118', instrument_id: 4, bio: 'hi my name is Madonna', location_id: 2, image_url: '1.jpg'},
       ])
     })
 }
